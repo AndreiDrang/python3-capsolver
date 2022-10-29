87047bb3d21f4baf68181c6f058630ca4629e36c
Andrei <drang.andray@gmail.com>
2022-10-29 07:25:32 +0300

    Update image_to_text.py

--- src/python3_captchaai/image_to_text.py
+++ src/python3_captchaai/image_to_text.py
@@ -1,13 +1,15 @@
+from typing import Union
+
 from src.python3_captchaai.core.base import BaseCaptcha
 from src.python3_captchaai.core.enums import CaptchaTypeEnm
-from src.python3_captchaai.core.serializer import ImageToTextTask, CaptchaResponseSer
+from src.python3_captchaai.core.serializer import ImageToTextTask, CaptchaResponseSer, CreateTaskResponseSer
 
 
-class BaseControl(BaseCaptcha):
+class BaseImageToText(BaseCaptcha):
     pass
 
 
-class ImageToText(BaseControl):
+class ImageToText(BaseImageToText):
     """
     The class is used to work with CaptchaAI control methods.
 
@@ -15,9 +17,14 @@ class ImageToText(BaseControl):
         https://captchaai.atlassian.net/wiki/spaces/CAPTCHAAI/pages/393427/ImageToTextTask+beta+solve+image+captcha
     """
 
-    def captcha_handler(self, body, **additional_params) -> CaptchaResponseSer:
+    def captcha_handler(self, body, **additional_params) -> Union[CaptchaResponseSer, CreateTaskResponseSer]:
         """
         Synchronous method for captcha solving
+
+        Args:
+            body: Base64 encoded content of the image
+            additional_params: Some additional parameters that will be used in creating the task
+                                and will be passed to the payload
 
         Examples:
             >>> ImageToText(api_key="CAI-12345").captcha_handler()
