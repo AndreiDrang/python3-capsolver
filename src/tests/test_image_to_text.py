1d151d73aa43c2c113c2ea80683cd3ca96d32a19
Andrei <drang.andray@gmail.com>
2022-11-05 17:11:06 +0300

    Update test_image_to_text.py

--- src/tests/test_image_to_text.py
+++ src/tests/test_image_to_text.py
@@ -1,6 +1,4 @@
-import os
 import base64
-import logging
 
 import pytest
 
@@ -9,28 +7,26 @@ from python3_captchaai.core.enums import ResponseStatusEnm
 from python3_captchaai.image_to_text import ImageToText
 from python3_captchaai.core.serializer import CaptchaResponseSer
 
-print(os.getcwd())
-logging.warning(os.getcwd())
 with open("tests/files/captcha_example.jpeg", "rb") as img_file:
     img_data = img_file.read()
 
 
-class TestControl(BaseTest):
+class TestImageToText(BaseTest):
     image_body = base64.b64encode(img_data).decode("utf-8")
     """
     Success tests
     """
 
-    def test_get_balance_exist(self):
+    def test_captcha_handler_exist(self):
         assert "captcha_handler" in ImageToText.__dict__.keys()
 
-    def test_aio_get_balance_exist(self):
+    def test_aio_captcha_handler_exist(self):
         assert "aio_captcha_handler" in ImageToText.__dict__.keys()
 
     def test_solve_image(self):
         resp = ImageToText(api_key=self.API_KEY).captcha_handler(body=self.image_body)
         assert isinstance(resp, CaptchaResponseSer)
-        assert resp.state == ResponseStatusEnm.Ready
+        assert resp.status == ResponseStatusEnm.Ready
         assert resp.errorId is False
         assert resp.ErrorCode is None
         assert resp.errorDescription is None
@@ -38,7 +34,7 @@ class TestControl(BaseTest):
     async def test_aio_solve_image(self):
         resp = await ImageToText(api_key=self.API_KEY).aio_captcha_handler(body=self.image_body)
         assert isinstance(resp, CaptchaResponseSer)
-        assert resp.state == ResponseStatusEnm.Ready
+        assert resp.status == ResponseStatusEnm.Ready
         assert resp.errorId is False
         assert resp.ErrorCode is None
         assert resp.errorDescription is None
@@ -47,10 +43,10 @@ class TestControl(BaseTest):
     Failed tests
     """
 
-    def test_get_balance_api_key_err(self):
+    def test_captcha_handler_api_key_err(self):
         with pytest.raises(Exception):
             ImageToText(api_key=self.get_random_string(36)).captcha_handler(body=self.image_body)
 
-    async def test_aio_get_balance_api_key_err(self):
+    async def test_aio_captcha_handler_api_key_err(self):
         with pytest.raises(Exception):
             await ImageToText(api_key=self.get_random_string(36)).aio_captcha_handler(body=self.image_body)
