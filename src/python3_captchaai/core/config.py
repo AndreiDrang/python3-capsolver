7acc374111f4e00964229ce7028dd3fafb17e7fa
Andrei <drang.andray@gmail.com>
2022-11-05 17:39:43 +0300

    Update config.py

--- src/python3_captchaai/core/config.py
+++ src/python3_captchaai/core/config.py
@@ -1,6 +1,8 @@
+from typing import Generator
+
 from tenacity import AsyncRetrying, wait_fixed, stop_after_attempt
 from requests.adapters import Retry
-from typing import Generator
+
 RETRIES = Retry(total=5, backoff_factor=0.9, status_forcelist=[500, 502, 503, 504])
 ASYNC_RETRIES = AsyncRetrying(wait=wait_fixed(5), stop=stop_after_attempt(5), reraise=True)
 
