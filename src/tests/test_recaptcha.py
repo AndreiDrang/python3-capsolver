74eaeadceab9fe1cb8843ec14d0c0f9ec2431376
Andrei <drang.andray@gmail.com>
2022-11-05 22:04:52 +0300

    Update test_recaptcha.py

--- src/tests/test_recaptcha.py
+++ src/tests/test_recaptcha.py
@@ -5,24 +5,40 @@ from python3_captchaai.recaptcha import ReCaptcha
 from python3_captchaai.core.enums import CaptchaTypeEnm, ResponseStatusEnm
 from python3_captchaai.core.serializer import CaptchaResponseSer
 
+GOOGLE_KEY = "6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-"
+PAGE_URL = "https://www.google.com/recaptcha/api2/demo"
 
-class TestReCaptcha(BaseTest):
-    googlekey = "6LeIxboZAAAAAFQy7d8GPzgRZu2bV0GwKS8ue_cH"
-    pageurl = "https://rucaptcha.com/demo/recaptcha-v2"
-    """
-    Success tests
-    """
 
+class TestReCaptchaBase(BaseTest):
     def test_captcha_handler_exist(self):
         assert "captcha_handler" in ReCaptcha.__dict__.keys()
 
     def test_aio_captcha_handler_exist(self):
         assert "aio_captcha_handler" in ReCaptcha.__dict__.keys()
 
-    def test_solve_v2_task_proxy_less(self):
+    def test_wrong_captcha_type(self):
+        with pytest.raises(ValueError):
+            ReCaptcha(
+                api_key=self.get_random_string(36),
+                captcha_type=CaptchaTypeEnm.Control,
+                websiteURL=self.get_random_string(5),
+                websiteKey=self.get_random_string(5),
+            )
+
+
+class TestReCaptchaV2ProxyLess(BaseTest):
+    googlekey = GOOGLE_KEY
+    pageurl = PAGE_URL
+
+    captcha_type = CaptchaTypeEnm.ReCaptchaV2TaskProxyLess
+    """
+    Success tests
+    """
+
+    def test_solve(self):
         resp = ReCaptcha(
             api_key=self.API_KEY,
-            captcha_type=CaptchaTypeEnm.ReCaptchaV2TaskProxyLess,
+            captcha_type=self.captcha_type,
             websiteURL=self.pageurl,
             websiteKey=self.googlekey,
         ).captcha_handler()
@@ -33,10 +49,10 @@ class TestReCaptcha(BaseTest):
         assert resp.errorDescription is None
         assert resp.solution is not None
 
-    def test_solve_v2_task_proxy_less_context(self):
+    def test_solve_context(self):
         with ReCaptcha(
             api_key=self.API_KEY,
-            captcha_type=CaptchaTypeEnm.ReCaptchaV2TaskProxyLess,
+            captcha_type=self.captcha_type,
             websiteURL=self.pageurl,
             websiteKey=self.googlekey,
         ) as instance:
@@ -48,10 +64,10 @@ class TestReCaptcha(BaseTest):
         assert resp.errorDescription is None
         assert resp.solution is not None
 
-    async def test_aio_solve_v2_task_proxy_less(self):
+    async def test_aio_solve(self):
         resp = await ReCaptcha(
             api_key=self.API_KEY,
-            captcha_type=CaptchaTypeEnm.ReCaptchaV2TaskProxyLess,
+            captcha_type=self.captcha_type,
             websiteURL=self.pageurl,
             websiteKey=self.googlekey,
         ).aio_captcha_handler()
@@ -62,10 +78,10 @@ class TestReCaptcha(BaseTest):
         assert resp.errorDescription is None
         assert resp.solution is not None
 
-    async def test_aio_solve_v2_task_proxy_less_context(self):
+    async def test_aio_solve_context(self):
         with ReCaptcha(
             api_key=self.API_KEY,
-            captcha_type=CaptchaTypeEnm.ReCaptchaV2TaskProxyLess,
+            captcha_type=self.captcha_type,
             websiteURL=self.pageurl,
             websiteKey=self.googlekey,
         ) as instance:
@@ -81,20 +97,11 @@ class TestReCaptcha(BaseTest):
     Failed tests
     """
 
-    def test_captcha_handler_api_key_err(self):
+    def test_api_key_err(self):
         with pytest.raises(Exception):
             ReCaptcha(
                 api_key=self.get_random_string(36),
-                captcha_type=CaptchaTypeEnm.ReCaptchaV2TaskProxyLess,
+                captcha_type=self.captcha_type,
                 websiteURL=self.pageurl,
                 websiteKey=self.googlekey,
-            ).captcha_handler()
-
-    async def test_aio_captcha_handler_api_key_err(self):
-        with pytest.raises(Exception):
-            await ReCaptcha(
-                api_key=self.get_random_string(36),
-                captcha_type=CaptchaTypeEnm.ReCaptchaV2TaskProxyLess,
-                websiteURL=self.pageurl,
-                websiteKey=self.googlekey,
-            ).aio_captcha_handler()
+            )
