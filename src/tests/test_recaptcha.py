19d194be1b0ce487442b8e60a4a1d9dcd611f1b5
Andrei <drang.andray@gmail.com>
2022-11-05 19:17:59 +0300

    Create test_recaptcha.py

---
+++ src/tests/test_recaptcha.py
@@ -0,0 +1,58 @@
+import pytest
+
+from src.tests.conftest import BaseTest
+from python3_captchaai.recaptcha import ReCaptcha
+from python3_captchaai.core.enums import CaptchaTypeEnm, ResponseStatusEnm
+from python3_captchaai.core.serializer import CaptchaResponseSer
+
+
+class TestReCaptcha(BaseTest):
+    googlekey = "6LeIxboZAAAAAFQy7d8GPzgRZu2bV0GwKS8ue_cH"
+    pageurl = "https://rucaptcha.com/demo/recaptcha-v2"
+    """
+    Success tests
+    """
+
+    def test_captcha_handler_exist(self):
+        assert "captcha_handler" in ReCaptcha.__dict__.keys()
+
+    def test_aio_captcha_handler_exist(self):
+        assert "aio_captcha_handler" in ReCaptcha.__dict__.keys()
+
+    def test_solve_v2_task_proxy_less(self):
+        resp = ReCaptcha(api_key=self.API_KEY, captcha_type=CaptchaTypeEnm.ReCaptchaV2TaskProxyLess).captcha_handler(
+            websiteURL=self.pageurl, websiteKey=self.googlekey
+        )
+        assert isinstance(resp, CaptchaResponseSer)
+        assert resp.status == ResponseStatusEnm.Ready
+        assert resp.errorId is False
+        assert resp.ErrorCode is None
+        assert resp.errorDescription is None
+        assert resp.solution is not None
+
+    async def test_aio_solve_v2_task_proxy_less(self):
+        resp = await ReCaptcha(
+            api_key=self.API_KEY, captcha_type=CaptchaTypeEnm.ReCaptchaV2TaskProxyLess
+        ).aio_captcha_handler(websiteURL=self.pageurl, websiteKey=self.googlekey)
+        assert isinstance(resp, CaptchaResponseSer)
+        assert resp.status == ResponseStatusEnm.Ready
+        assert resp.errorId is False
+        assert resp.ErrorCode is None
+        assert resp.errorDescription is None
+        assert resp.solution is not None
+
+    """
+    Failed tests
+    """
+
+    def test_captcha_handler_api_key_err(self):
+        with pytest.raises(Exception):
+            ReCaptcha(api_key=self.get_random_string(36)).captcha_handler(
+                websiteURL=self.pageurl, websiteKey=self.googlekey
+            )
+
+    async def test_aio_captcha_handler_api_key_err(self):
+        with pytest.raises(Exception):
+            await ReCaptcha(api_key=self.get_random_string(36)).aio_captcha_handler(
+                websiteURL=self.pageurl, websiteKey=self.googlekey
+            )
