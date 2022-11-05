d98958a9f8f1b688c5c3f711621d7f0c5521dda8
Andrei <drang.andray@gmail.com>
2022-11-05 20:53:44 +0300

    Update test_core.py

--- src/tests/test_core.py
+++ src/tests/test_core.py
@@ -4,6 +4,7 @@ from urllib3.util.retry import Retry
 
 from src.tests.conftest import BaseTest
 from python3_captchaai.core.base import BaseCaptcha
+from python3_captchaai.core.enums import CaptchaTypeEnm
 from python3_captchaai.core.config import RETRIES, ASYNC_RETRIES
 
 
@@ -20,14 +21,24 @@ class TestCore(BaseTest):
 
     async def test_no_key_err(self):
         with pytest.raises(TypeError):
-            BaseCaptcha()
+            BaseCaptcha(captcha_type=CaptchaTypeEnm.ImageToTextTask)
+
+    async def test_no_captcha_type(self):
+        with pytest.raises(TypeError):
+            BaseCaptcha(api_key=self.get_random_string(36))
 
     @pytest.mark.parametrize("api_len", [35, 37])
     def test_wrong_key_err(self, api_len: int):
         with pytest.raises(ValueError):
-            BaseCaptcha(api_key=self.get_random_string(api_len))
+            BaseCaptcha(api_key=self.get_random_string(api_len), captcha_type=CaptchaTypeEnm.ImageToTextTask)
 
     @pytest.mark.parametrize("sleep_time", range(-2, 5))
     def test_wrong_sleep_time(self, sleep_time: int):
         with pytest.raises(ValueError):
-            BaseCaptcha(api_key=self.get_random_string(36), sleep_time=sleep_time)
+            BaseCaptcha(
+                api_key=self.get_random_string(36), sleep_time=sleep_time, captcha_type=CaptchaTypeEnm.ImageToTextTask
+            )
+
+    def test_wrong_captcha_type(self):
+        with pytest.raises(ValueError):
+            BaseCaptcha(api_key=self.get_random_string(36), captcha_type=self.get_random_string(10))
