0c88a74a6a1dcb360afc7b518b0e2d56e04eb1de
Andrei <drang.andray@gmail.com>
2022-11-05 22:56:27 +0300

    Update test_recaptcha.py

--- src/tests/test_recaptcha.py
+++ src/tests/test_recaptcha.py
@@ -1,9 +1,9 @@
 import pytest
+from pydantic import ValidationError
 
 from src.tests.conftest import BaseTest
 from python3_captchaai.recaptcha import ReCaptcha
-from python3_captchaai.core.enums import CaptchaTypeEnm, ResponseStatusEnm
-from python3_captchaai.core.serializer import CaptchaResponseSer
+from python3_captchaai.core.enums import ProxyType, CaptchaTypeEnm
 
 GOOGLE_KEY = "6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-"
 PAGE_URL = "https://www.google.com/recaptcha/api2/demo"
@@ -35,7 +35,19 @@ class TestReCaptchaV2ProxyLess(BaseTest):
     Success tests
     """
 
-    def test_solve(self):
+    def test_params(self):
+        ReCaptcha(
+            api_key=self.API_KEY, captcha_type=self.captcha_type, websiteURL=self.pageurl, websiteKey=self.googlekey
+        )
+
+    def test_params_context(self):
+        with ReCaptcha(
+            api_key=self.API_KEY, captcha_type=self.captcha_type, websiteURL=self.pageurl, websiteKey=self.googlekey
+        ) as instance:
+            pass
+
+    # TODO start use it, temporary Captcha not solving
+    """def test_solve(self):
         resp = ReCaptcha(
             api_key=self.API_KEY,
             captcha_type=self.captcha_type,
@@ -91,11 +103,19 @@ class TestReCaptchaV2ProxyLess(BaseTest):
         assert resp.errorId is False
         assert resp.ErrorCode is None
         assert resp.errorDescription is None
-        assert resp.solution is not None
+        assert resp.solution is not None"""
 
     """
     Failed tests
     """
+
+    def test_no_website_key(self):
+        with pytest.raises(TypeError):
+            ReCaptcha(api_key=self.API_KEY, captcha_type=self.captcha_type, websiteURL=self.pageurl)
+
+    def test_no_website_url(self):
+        with pytest.raises(TypeError):
+            ReCaptcha(api_key=self.API_KEY, captcha_type=self.captcha_type, websiteKey=self.googlekey)
 
     def test_api_key_err(self):
         with pytest.raises(Exception):
@@ -104,4 +124,97 @@ class TestReCaptchaV2ProxyLess(BaseTest):
                 captcha_type=self.captcha_type,
                 websiteURL=self.pageurl,
                 websiteKey=self.googlekey,
+            ).captcha_handler()
+
+    async def test_aio_api_key_err(self):
+        with pytest.raises(Exception):
+            await ReCaptcha(
+                api_key=self.get_random_string(36),
+                captcha_type=self.captcha_type,
+                websiteURL=self.pageurl,
+                websiteKey=self.googlekey,
+            ).aio_captcha_handler()
+
+
+class TestReCaptchaV2(BaseTest):
+    googlekey = GOOGLE_KEY
+    pageurl = PAGE_URL
+    proxyAddress = "0.0.0.0"
+    proxyPort = 9999
+
+    captcha_type = CaptchaTypeEnm.ReCaptchaV2Task
+    """
+    Success tests
+    """
+
+    @pytest.mark.parametrize("proxy_type", ProxyType.list_values())
+    def test_params(self, proxy_type: str):
+        ReCaptcha(
+            api_key=self.API_KEY,
+            captcha_type=self.captcha_type,
+            websiteURL=self.pageurl,
+            websiteKey=self.googlekey,
+            proxyAddress=self.proxyAddress,
+            proxyType=proxy_type,
+            proxyPort=self.proxyPort,
+        )
+
+    @pytest.mark.parametrize("proxy_type", ProxyType.list_values())
+    def test_params_context(self, proxy_type: str):
+        with ReCaptcha(
+            api_key=self.API_KEY,
+            captcha_type=self.captcha_type,
+            websiteURL=self.pageurl,
+            websiteKey=self.googlekey,
+            proxyAddress=self.proxyAddress,
+            proxyType=proxy_type,
+            proxyPort=self.proxyPort,
+        ) as instance:
+            pass
+
+    """
+    Failed tests
+    """
+
+    def test_no_website_key(self):
+        with pytest.raises(TypeError):
+            ReCaptcha(api_key=self.API_KEY, captcha_type=self.captcha_type, websiteURL=self.pageurl)
+
+    def test_no_website_url(self):
+        with pytest.raises(TypeError):
+            ReCaptcha(api_key=self.API_KEY, captcha_type=self.captcha_type, websiteKey=self.googlekey)
+
+    def test_no_proxy_type(self):
+        with pytest.raises(ValidationError):
+            ReCaptcha(
+                api_key=self.API_KEY,
+                captcha_type=self.captcha_type,
+                websiteURL=self.pageurl,
+                websiteKey=self.googlekey,
+                proxyAddress=self.proxyAddress,
+                proxyPort=self.proxyPort,
+            )
+
+    @pytest.mark.parametrize("proxy_type", ProxyType.list_values())
+    def test_no_proxy_address(self, proxy_type: str):
+        with pytest.raises(ValidationError):
+            ReCaptcha(
+                api_key=self.API_KEY,
+                captcha_type=self.captcha_type,
+                websiteURL=self.pageurl,
+                websiteKey=self.googlekey,
+                proxyType=proxy_type,
+                proxyPort=self.proxyPort,
+            )
+
+    @pytest.mark.parametrize("proxy_type", ProxyType.list_values())
+    def test_no_proxy_port(self, proxy_type):
+        with pytest.raises(ValidationError):
+            ReCaptcha(
+                api_key=self.API_KEY,
+                captcha_type=self.captcha_type,
+                websiteURL=self.pageurl,
+                websiteKey=self.googlekey,
+                proxyAddress=self.proxyAddress,
+                proxyType=proxy_type,
             )
