8d3139a8f25975ee2d4e4232d38e864b34f11377
Andrei <drang.andray@gmail.com>
2022-11-06 06:26:10 +0300

    0.0.5

--- src/setup.py
+++ src/setup.py
@@ -14,7 +14,7 @@ URL = "https://github.com/AndreiDrang/python3-captchaai.git"
 EMAIL = "python-captcha@pm.me"
 AUTHOR = "AndreiDrang"
 REQUIRES_PYTHON = ">=3.7.0"
-VERSION = "0.0.4"
+VERSION = "0.0.5"
 with open("requirements.txt", "rt") as requirements_txt:
     REQUIRED = [str(requirement) for requirement in parse_requirements(requirements_txt)]
 
