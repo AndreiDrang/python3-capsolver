c9b46811023ff4890c9750a295e87cdd02482a72
Andrei <drang.andray@gmail.com>
2022-11-11 03:50:16 +0300

    fix geetest && added more tests

--- src/tests/test_gee_test.py
+++ src/tests/test_gee_test.py
@@ -1,4 +1,5 @@
 import pytest
+from pydantic import ValidationError
 
 from src.tests.conftest import BaseTest
 from python3_captchaai.gee_test import GeeTest
@@ -8,6 +9,8 @@ PAGE_URL = "https://www.geetest.com/en/demo"
 GT = "022397c99c9f646f6477822485f30404"
 CHALLENGE = "a66f31a53a404af8d1f271eec5138aa1"
 API_SUBDOMAIN = "api.geetest.com"
+
+captcha_types = (CaptchaTypeEnm.GeetestTask, CaptchaTypeEnm.GeetestTaskProxyless)
 
 
 class TestGeeTestBase(BaseTest):
@@ -22,9 +25,27 @@ class TestGeeTestBase(BaseTest):
             GeeTest(
                 api_key=self.get_random_string(36),
                 captcha_type=CaptchaTypeEnm.Control,
-                websiteURL="https://www.geetest.com/en/demo",
-                gt="022397c99c9f646f6477822485f30404",
+                websiteURL=PAGE_URL,
+                gt=GT,
             )
+
+    def test_wrong_type_params(self):
+        with pytest.raises(ValidationError):
+            GeeTest(
+                api_key=self.get_random_string(36),
+                captcha_type=CaptchaTypeEnm.GeetestTask,
+                websiteURL=PAGE_URL,
+                gt=GT,
+            ).captcha_handler(challenge=CHALLENGE)
+
+    async def test_aio_wrong_type_params(self):
+        with pytest.raises(ValidationError):
+            await GeeTest(
+                api_key=self.get_random_string(36),
+                captcha_type=CaptchaTypeEnm.GeetestTask,
+                websiteURL=PAGE_URL,
+                gt=GT,
+            ).aio_captcha_handler(challenge=CHALLENGE)
 
     def test_no_captcha_type(self):
         with pytest.raises(TypeError):
