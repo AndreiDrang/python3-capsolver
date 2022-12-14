cb05749e575856261136d98f7950c7c628e447ec
Andrei <drang.andray@gmail.com>
2022-12-14 19:27:40 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,18 +1,8 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Path setup --------------------------------------------------------------
 
 import os
 import sys
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 from pallets_sphinx_themes import ProjectLink
 
 os.chdir("../")
