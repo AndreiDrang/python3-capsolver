d9fd6855c4c11204c80e5ba73eb02821c895a5e2
Andrei <drang.andray@gmail.com>
2023-07-27 20:03:54 +0300

    Update test_fun_captcha.py

--- tests/test_fun_captcha.py
+++ tests/test_fun_captcha.py
@@ -4,8 +4,8 @@ import pytest
 from pydantic import ValidationError
 
 from tests.conftest import BaseTest
-from python3_capsolver.core.enum import ResponseStatusEnm
-from python3_capsolver.fun_captcha import FunCaptcha, FunCaptchaTypeEnm
+from python3_capsolver.core.enum import FunCaptchaTypeEnm, ResponseStatusEnm, FunCaptchaClassificationTypeEnm
+from python3_capsolver.fun_captcha import FunCaptcha, FunCaptchaClassification
 from python3_capsolver.core.serializer import CaptchaResponseSer
 
 websiteURL = "https://api.funcaptcha.com/fc/api/nojs/"
@@ -33,15 +33,23 @@ class TestFunCaptchaBase(BaseTest):
                 funcaptchaApiJSSubdomain=funcaptchaApiJSSubdomain,
             ).captcha_handler()
 
-    @pytest.mark.parametrize("captcha_type", FunCaptchaTypeEnm.list_values())
-    def test_wrong_type_params(self, captcha_type: str):
+    def test_wrong_type_params(self):
         with pytest.raises(expected_exception=(ValidationError, ValueError)):
-            FunCaptcha(api_key=self.get_random_string(36), captcha_type=captcha_type).captcha_handler()
+            FunCaptcha(
+                api_key=self.get_random_string(36),
+                captcha_type=self.get_random_string(5),
+                websiteURL=websiteURL,
+                websitePublicKey=websitePublicKey,
+            ).captcha_handler()
 
-    @pytest.mark.parametrize("captcha_type", FunCaptchaTypeEnm.list_values())
-    async def test_aio_wrong_type_params(self, captcha_type: str):
+    async def test_aio_wrong_type_params(self):
         with pytest.raises(expected_exception=(ValidationError, ValueError)):
-            await FunCaptcha(api_key=self.get_random_string(36), captcha_type=captcha_type).aio_captcha_handler()
+            await FunCaptcha(
+                api_key=self.get_random_string(36),
+                captcha_type=self.get_random_string(5),
+                websiteURL=websiteURL,
+                websitePublicKey=websitePublicKey,
+            ).aio_captcha_handler()
 
     def test_no_captcha_type(self):
         with pytest.raises(TypeError):
@@ -78,70 +86,70 @@ class TestFunCaptchaProxyless(BaseTest):
         ) as instance:
             pass
 
-    def test_solve(self):
-        resp = FunCaptcha(
-            api_key=self.API_KEY,
-            captcha_type=self.captcha_type,
-            websiteURL=websiteURL,
-            websitePublicKey=websitePublicKey,
-            funcaptchaApiJSSubdomain=funcaptchaApiJSSubdomain,
-        ).captcha_handler()
-        assert isinstance(resp, CaptchaResponseSer)
-        assert resp.status in (ResponseStatusEnm.Ready, ResponseStatusEnm.Processing)
-        assert resp.errorId in (False, True)
-        assert resp.errorCode in (None, "ERROR_ZERO_BALANCE")
-        assert resp.errorDescription in (None, "Your service balance is insufficient.")
-
-    def test_solve_context(self):
-        with FunCaptcha(
-            api_key=self.API_KEY,
-            captcha_type=self.captcha_type,
-            websiteURL=websiteURL,
-            websitePublicKey=websitePublicKey,
-            funcaptchaApiJSSubdomain=funcaptchaApiJSSubdomain,
-        ) as instance:
-            resp = instance.captcha_handler()
+    """    def test_solve(self):
+            resp = FunCaptcha(
+                api_key=self.API_KEY,
+                captcha_type=self.captcha_type,
+                websiteURL=websiteURL,
+                websitePublicKey=websitePublicKey,
+                funcaptchaApiJSSubdomain=funcaptchaApiJSSubdomain,
+            ).captcha_handler()
             assert isinstance(resp, CaptchaResponseSer)
             assert resp.status in (ResponseStatusEnm.Ready, ResponseStatusEnm.Processing)
             assert resp.errorId in (False, True)
             assert resp.errorCode in (None, "ERROR_ZERO_BALANCE")
             assert resp.errorDescription in (None, "Your service balance is insufficient.")
 
-    async def test_aio_solve(self):
-        resp = await FunCaptcha(
-            api_key=self.API_KEY,
-            captcha_type=self.captcha_type,
-            websiteURL=websiteURL,
-            websitePublicKey=websitePublicKey,
-            funcaptchaApiJSSubdomain=funcaptchaApiJSSubdomain,
-        ).aio_captcha_handler()
-        assert isinstance(resp, CaptchaResponseSer)
-        assert resp.status in (ResponseStatusEnm.Ready, ResponseStatusEnm.Processing)
-        assert resp.errorId in (False, True)
-        assert resp.errorCode in (None, "ERROR_ZERO_BALANCE")
-        assert resp.errorDescription in (None, "Your service balance is insufficient.")
+        def test_solve_context(self):
+            with FunCaptcha(
+                api_key=self.API_KEY,
+                captcha_type=self.captcha_type,
+                websiteURL=websiteURL,
+                websitePublicKey=websitePublicKey,
+                funcaptchaApiJSSubdomain=funcaptchaApiJSSubdomain,
+            ) as instance:
+                resp = instance.captcha_handler()
+                assert isinstance(resp, CaptchaResponseSer)
+                assert resp.status in (ResponseStatusEnm.Ready, ResponseStatusEnm.Processing)
+                assert resp.errorId in (False, True)
+                assert resp.errorCode in (None, "ERROR_ZERO_BALANCE")
+                assert resp.errorDescription in (None, "Your service balance is insufficient.")
 
-    async def test_aio_solve_context(self):
-        with FunCaptcha(
-            api_key=self.API_KEY,
-            captcha_type=self.captcha_type,
-            websiteURL=websiteURL,
-            websitePublicKey=websitePublicKey,
-            funcaptchaApiJSSubdomain=funcaptchaApiJSSubdomain,
-        ) as instance:
-            resp = await instance.aio_captcha_handler()
+        async def test_aio_solve(self):
+            resp = await FunCaptcha(
+                api_key=self.API_KEY,
+                captcha_type=self.captcha_type,
+                websiteURL=websiteURL,
+                websitePublicKey=websitePublicKey,
+                funcaptchaApiJSSubdomain=funcaptchaApiJSSubdomain,
+            ).aio_captcha_handler()
             assert isinstance(resp, CaptchaResponseSer)
             assert resp.status in (ResponseStatusEnm.Ready, ResponseStatusEnm.Processing)
             assert resp.errorId in (False, True)
             assert resp.errorCode in (None, "ERROR_ZERO_BALANCE")
             assert resp.errorDescription in (None, "Your service balance is insufficient.")
 
+        async def test_aio_solve_context(self):
+            with FunCaptcha(
+                api_key=self.API_KEY,
+                captcha_type=self.captcha_type,
+                websiteURL=websiteURL,
+                websitePublicKey=websitePublicKey,
+                funcaptchaApiJSSubdomain=funcaptchaApiJSSubdomain,
+            ) as instance:
+                resp = await instance.aio_captcha_handler()
+                assert isinstance(resp, CaptchaResponseSer)
+                assert resp.status in (ResponseStatusEnm.Ready, ResponseStatusEnm.Processing)
+                assert resp.errorId in (False, True)
+                assert resp.errorCode in (None, "ERROR_ZERO_BALANCE")
+                assert resp.errorDescription in (None, "Your service balance is insufficient.")
+    """
     """
     Failed tests
     """
 
     def test_no_website_key(self):
-        with pytest.raises(ValidationError):
+        with pytest.raises(TypeError):
             FunCaptcha(
                 api_key=self.API_KEY,
                 captcha_type=self.captcha_type,
@@ -150,64 +158,69 @@ class TestFunCaptchaProxyless(BaseTest):
             )
 
     def test_no_website_url(self):
-        with pytest.raises(ValidationError):
+        with pytest.raises(TypeError):
             FunCaptcha(
                 api_key=self.API_KEY,
                 captcha_type=self.captcha_type,
                 websitePublicKey=websitePublicKey,
                 funcaptchaApiJSSubdomain=funcaptchaApiJSSubdomain,
-            )
-
-    def test_no_api_js(self):
-        with pytest.raises(ValidationError):
-            FunCaptcha(
-                api_key=self.API_KEY,
-                captcha_type=self.captcha_type,
-                websiteURL=websiteURL,
-                websitePublicKey=websitePublicKey,
             )
 
     async def test_aio_api_key_err(self):
-        with pytest.raises(Exception):
-            await FunCaptcha(
-                api_key=self.get_random_string(36),
-                captcha_type=self.captcha_type,
-                websiteURL=websiteURL,
-                websitePublicKey=websitePublicKey,
-                funcaptchaApiJSSubdomain=funcaptchaApiJSSubdomain,
-            ).aio_captcha_handler()
+        result = await FunCaptcha(
+            api_key=self.get_random_string(36),
+            captcha_type=self.captcha_type,
+            websiteURL=websiteURL,
+            websitePublicKey=websitePublicKey,
+            funcaptchaApiJSSubdomain=funcaptchaApiJSSubdomain,
+        ).aio_captcha_handler()
+        assert result.errorId == 1
+        assert result.errorCode == "ERROR_KEY_DENIED_ACCESS"
+        assert not result.solution
 
     def test_api_key_err(self):
-        with pytest.raises(Exception):
-            FunCaptcha(
-                api_key=self.get_random_string(36),
-                captcha_type=self.captcha_type,
-                websiteURL=websiteURL,
-                websitePublicKey=websitePublicKey,
-                funcaptchaApiJSSubdomain=funcaptchaApiJSSubdomain,
-            ).captcha_handler()
+        result = FunCaptcha(
+            api_key=self.get_random_string(36),
+            captcha_type=self.captcha_type,
+            websiteURL=websiteURL,
+            websitePublicKey=websitePublicKey,
+            funcaptchaApiJSSubdomain=funcaptchaApiJSSubdomain,
+        ).captcha_handler()
+        assert result.errorId == 1
+        assert result.errorCode == "ERROR_KEY_DENIED_ACCESS"
+        assert not result.solution
 
 
 class TestFunCaptchaClassification(BaseTest):
-    captcha_type = FunCaptchaTypeEnm.FunCaptchaClassification
+    captcha_type = FunCaptchaClassificationTypeEnm.FunCaptchaClassification
 
-    question = "Pick the bicycle"
-    image = base64.b64encode(img_data).decode("utf-8")
+    question = "maze"
+    images = [base64.b64encode(img_data).decode("utf-8")]
     """
     Success tests
     """
 
     def test_params(self):
-        FunCaptcha(api_key=self.get_random_string(36), captcha_type=self.captcha_type)
+        FunCaptchaClassification(
+            api_key=self.get_random_string(36),
+            captcha_type=self.captcha_type,
+            images=self.images,
+            question=self.question,
+        )
 
     def test_params_context(self):
-        with FunCaptcha(api_key=self.get_random_string(36), captcha_type=self.captcha_type) as instance:
+        with FunCaptchaClassification(
+            api_key=self.get_random_string(36),
+            captcha_type=self.captcha_type,
+            images=self.images,
+            question=self.question,
+        ) as instance:
             pass
 
     def test_solve(self):
-        resp = FunCaptcha(api_key=self.API_KEY, captcha_type=self.captcha_type).captcha_handler(
-            image=self.image, question=self.question
-        )
+        resp = FunCaptchaClassification(
+            api_key=self.API_KEY, captcha_type=self.captcha_type, images=self.images, question=self.question
+        ).captcha_handler()
         assert isinstance(resp, CaptchaResponseSer)
         assert resp.status == ResponseStatusEnm.Processing
         assert resp.errorId == 1
@@ -217,8 +230,10 @@ class TestFunCaptchaClassification(BaseTest):
         assert resp.solution is not None"""
 
     def test_solve_context(self):
-        with FunCaptcha(api_key=self.API_KEY, captcha_type=self.captcha_type) as instance:
-            resp = instance.captcha_handler(image=self.image, question=self.question)
+        with FunCaptchaClassification(
+            api_key=self.API_KEY, captcha_type=self.captcha_type, images=self.images, question=self.question
+        ) as instance:
+            resp = instance.captcha_handler()
             assert isinstance(resp, CaptchaResponseSer)
             assert resp.status == ResponseStatusEnm.Processing
             assert resp.errorId == 1
@@ -228,9 +243,9 @@ class TestFunCaptchaClassification(BaseTest):
             assert resp.solution is not None"""
 
     async def test_aio_solve(self):
-        resp = await FunCaptcha(api_key=self.API_KEY, captcha_type=self.captcha_type).aio_captcha_handler(
-            image=self.image, question=self.question
-        )
+        resp = await FunCaptchaClassification(
+            api_key=self.API_KEY, captcha_type=self.captcha_type, images=self.images, question=self.question
+        ).aio_captcha_handler()
         assert isinstance(resp, CaptchaResponseSer)
         assert resp.status == ResponseStatusEnm.Processing
         assert resp.errorId == 1
@@ -240,8 +255,10 @@ class TestFunCaptchaClassification(BaseTest):
         assert resp.solution is not None"""
 
     async def test_aio_solve_context(self):
-        with FunCaptcha(api_key=self.API_KEY, captcha_type=self.captcha_type) as instance:
-            resp = await instance.aio_captcha_handler(image=self.image, question=self.question)
+        with FunCaptchaClassification(
+            api_key=self.API_KEY, captcha_type=self.captcha_type, images=self.images, question=self.question
+        ) as instance:
+            resp = await instance.aio_captcha_handler()
             assert isinstance(resp, CaptchaResponseSer)
             assert resp.status == ResponseStatusEnm.Processing
             assert resp.errorId == 1
@@ -255,19 +272,31 @@ class TestFunCaptchaClassification(BaseTest):
     """
 
     def test_no_image(self):
-        with pytest.raises(ValueError):
-            FunCaptcha(api_key=self.API_KEY, captcha_type=self.captcha_type).captcha_handler(question=self.question)
+        with pytest.raises(TypeError):
+            FunCaptchaClassification(
+                api_key=self.API_KEY, captcha_type=self.captcha_type, question=self.question
+            ).captcha_handler()
 
     def test_no_question(self):
-        with pytest.raises(ValueError):
-            FunCaptcha(api_key=self.API_KEY, captcha_type=self.captcha_type).captcha_handler(image=self.image)
+        with pytest.raises(TypeError):
+            FunCaptchaClassification(
+                api_key=self.API_KEY, captcha_type=self.captcha_type, images=self.images
+            ).captcha_handler()
 
-    async def test_aio_no_image(self):
-        with pytest.raises(ValueError):
-            await FunCaptcha(api_key=self.API_KEY, captcha_type=self.captcha_type).aio_captcha_handler(
-                question=self.question
-            )
+    def test_wrong_type_params(self):
+        with pytest.raises(expected_exception=(ValidationError, ValueError)):
+            FunCaptchaClassification(
+                api_key=self.API_KEY,
+                captcha_type=self.get_random_string(36),
+                images=self.images,
+                question=self.question,
+            ).captcha_handler()
 
-    async def test_aio_no_question(self):
-        with pytest.raises(ValueError):
-            await FunCaptcha(api_key=self.API_KEY, captcha_type=self.captcha_type).aio_captcha_handler(image=self.image)
+    async def test_aio_wrong_type_params(self):
+        with pytest.raises(expected_exception=(ValidationError, ValueError)):
+            FunCaptchaClassification(
+                api_key=self.API_KEY,
+                captcha_type=self.get_random_string(36),
+                images=self.images,
+                question=self.question,
+            ).captcha_handler()
