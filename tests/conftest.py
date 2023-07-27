d79acb19440efa5e442637cc62e091fa63679e72
Andrei <drang.andray@gmail.com>
2023-07-27 18:07:03 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,12 +8,12 @@ import pytest
 
 @pytest.fixture(scope="function")
 def delay_func():
-    time.sleep(0.1)
+    time.sleep(0.5)
 
 
 @pytest.fixture(scope="class")
 def delay_class():
-    time.sleep(0.1)
+    time.sleep(1.1)
 
 
 @pytest.mark.usefixtures("delay_func")
