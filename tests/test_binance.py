065e0ab07b9b7b113998048d40e141089f7920b8
Andrei <drang.andray@gmail.com>
2023-09-12 23:36:41 +0300

    Update test_binance.py

--- tests/test_binance.py
+++ tests/test_binance.py
@@ -4,6 +4,8 @@ from tests.conftest import BaseTest
 from python3_capsolver.binance import Binance
 from python3_capsolver.core.enum import BinanceCaptchaTaskEnm
 
+from python3_capsolver.core.serializer import CaptchaResponseSer, BinanceCaptchaTaskSer
+
 
 class TestBinanceBase(BaseTest):
     def test_captcha_handler_exist(self):
@@ -44,3 +46,35 @@ class TestBinanceBase(BaseTest):
                 captcha_type=BinanceCaptchaTaskEnm.BinanceCaptchaTask,
                 websiteURL=self.get_random_string(36),
             )
+
+
+class TestBinanceCaptchaTaskBase(BaseTest):
+    def test_instance(self):
+        instance = Binance(
+            api_key=self.get_random_string(36),
+            captcha_type=BinanceCaptchaTaskEnm.BinanceCaptchaTask,
+            websiteURL="https://www.milanuncios.com/",
+            validateId="3621a4fef82f4ab4a00e8b07465761c5",
+        )
+
+    def test_solve(self):
+        instance = Binance(
+            api_key=self.get_random_string(36),
+            captcha_type=BinanceCaptchaTaskEnm.BinanceCaptchaTask,
+            websiteURL="https://www.milanuncios.com/",
+            validateId="3621a4fef82f4ab4a00e8b07465761c5",
+            **{"proxyType": "socks5", "proxyAddress": "72.217.216.239", "proxyPort": 4145},
+        )
+        result = instance.captcha_handler()
+        assert isinstance(result, CaptchaResponseSer)
+
+    async def test_aio_solve(self):
+        instance = Binance(
+            api_key=self.get_random_string(36),
+            captcha_type=BinanceCaptchaTaskEnm.BinanceCaptchaTask,
+            websiteURL="https://www.milanuncios.com/",
+            validateId="3621a4fef82f4ab4a00e8b07465761c5",
+            **{"proxyType": "socks5", "proxyAddress": "72.217.216.239", "proxyPort": 4145},
+        )
+        result = await instance.aio_captcha_handler()
+        assert isinstance(result, CaptchaResponseSer)
