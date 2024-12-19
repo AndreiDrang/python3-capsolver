6b5302c8f7e7ea6e83ee46a0039ca241e7049b43
Andrei <drop.all.requests@gmail.com>
2024-12-19 20:26:59 +0300

    Update base.py

--- src/python3_capsolver/core/base.py
+++ src/python3_capsolver/core/base.py
@@ -2,6 +2,8 @@ from .const import REQUEST_URL
 from .serializer import RequestCreateTaskSer, RequestGetTaskResultSer
 from .context_instr import AIOContextManager, SIOContextManager
 from .captcha_instrument import CaptchaInstrument
+from .aio_captcha_instrument import AIOCaptchaInstrument
+from .sio_captcha_instrument import SIOCaptchaInstrument
 
 __all__ = ("CaptchaParams",)
 
@@ -33,6 +35,40 @@ class CaptchaParams(SIOContextManager, AIOContextManager):
         self.request_url = request_url
         self._captcha_handling_instrument = CaptchaInstrument()
 
-    """
-    Async part
-    """
+    def captcha_handler(self, **additional_params) -> dict:
+        """
+        Synchronous method for captcha solving
+
+        Args:
+            additional_params: Some additional parameters that will be used in creating the task
+                                and will be passed to the payload under ``task`` key.
+                                Like ``proxyLogin``, ``proxyPassword`` and etc. - more info in service docs
+
+        Returns:
+            Dict with full server response
+
+        Notes:
+            Check class docstirng for more info
+        """
+        self.task_params.update({**additional_params})
+        self._captcha_handling_instrument = SIOCaptchaInstrument(captcha_params=self)
+        return self._captcha_handling_instrument.processing_captcha()
+
+    async def aio_captcha_handler(self, **additional_params) -> dict:
+        """
+        Asynchronous method for captcha solving
+
+        Args:
+            additional_params: Some additional parameters that will be used in creating the task
+                                and will be passed to the payload under ``task`` key.
+                                Like ``proxyLogin``, ``proxyPassword`` and etc. - more info in service docs
+
+        Returns:
+            Dict with full server response
+
+        Notes:
+            Check class docstirng for more info
+        """
+        self.task_params.update({**additional_params})
+        self._captcha_handling_instrument = AIOCaptchaInstrument(captcha_params=self)
+        return await self._captcha_handling_instrument.processing_captcha()
