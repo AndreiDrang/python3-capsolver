4b216122478febc88d78b2076acf724e48826563
Andrei <drop.all.requests@gmail.com>
2025-01-17 23:31:02 +0300

    Update image_to_text.py

--- src/python3_capsolver/image_to_text.py
+++ src/python3_capsolver/image_to_text.py
@@ -1,8 +1,9 @@
-from typing import Union
+from typing import Dict
 
-from python3_capsolver.core.base import CaptchaParams
-from python3_capsolver.core.enum import ImageToTextTaskTypeEnm
-from python3_capsolver.core.serializer import TaskSer, CaptchaResponseSer
+from .core.base import CaptchaParams
+from .core.enum import CaptchaTypeEnm
+from .core.aio_captcha_instrument import AIOCaptchaInstrument
+from .core.sio_captcha_instrument import SIOCaptchaInstrument
 
 
 class ImageToText(CaptchaParams):
@@ -11,141 +12,112 @@ class ImageToText(CaptchaParams):
 
     Args:
         api_key: Capsolver API key
-        captcha_type: Captcha type name, like ``ImageToTextTask`` and etc.
 
     Examples:
-        >>> with open('some_image.jpeg', 'rb') as img_file:
-        ...    img_data = img_file.read()
-        >>> body = base64.b64encode(img_data).decode("utf-8")
-        >>> ImageToText(api_key="CAI-12345....").captcha_handler(body=body)
-        CaptchaResponseSer(errorId=0,
-                           errorCode=None,
-                           errorDescription=None,
-                           taskId='73bdcd28-6c77-4414-8....',
-                           status=<ResponseStatusEnm.Ready: 'ready'>,
-                           solution={'confidence': 0.9585, 'text': 'gcphjd'}
-                          )
+        >>> from python3_capsolver.image_to_text import ImageToText
+        >>> from python3_capsolver.core.captcha_instrument import FileInstrument
 
-        >>> with open('some_image.jpeg', 'rb') as img_file:
-        ...    img_data = img_file.read()
-        >>> body = base64.b64encode(img_data).decode("utf-8")
-        >>> ImageToText(api_key="CAI-12345....",
-        ...             module='queueit'
-        ...            ).captcha_handler(body=body)
-        CaptchaResponseSer(errorId=0,
-                           errorCode=None,
-                           errorDescription=None,
-                           taskId='73bdcd28-6c77-4414-8....',
-                           status=<ResponseStatusEnm.Ready: 'ready'>,
-                           solution={'confidence': 0.9585, 'text': 'zzzzz'}
-                          )
+        >>> body = FileInstrument().file_processing(captcha_file="captcha_example.jpeg")
+        >>> ImageToText(api_key="CAI-12345....")captcha_handler(
+        >>>                     task_payload={"body": body, "module": "common"}
+        >>>                     )
+        {
+           "errorId":0,
+           "errorCode":"None",
+           "errorDescription":"None",
+           "taskId":"db0a3153-621d-4f5e-8554-a1c032597ee7",
+           "status":"ready",
+           "solution":{
+              "confidence":0.9585,
+              "text":"gcphjd"
+           }
+        }
 
-        >>> with open('some_image.jpeg', 'rb') as img_file:
-        ...    img_data = img_file.read()
-        >>> body = base64.b64encode(img_data).decode("utf-8")
-        >>> ImageToText(api_key="CAI-12345....",
-        ...             module='dell',
-        ...             score=0.98,
-        ...             case=True,
-        ...            ).captcha_handler(body=body)
-        CaptchaResponseSer(errorId=0,
-                           errorCode=None,
-                           errorDescription=None,
-                           taskId='73bdcd28-6c77-4414-8....',
-                           status=<ResponseStatusEnm.Ready: 'ready'>,
-                           solution={'confidence': 0.9585, 'text': 'gcphjd'}
-                          )
+        >>> import asyncio
+        >>> from python3_capsolver.image_to_text import ImageToText
+        >>> from python3_capsolver.core.captcha_instrument import FileInstrument
 
-        >>> with open('some_image.jpeg', 'rb') as img_file:
-        ...    img_data = img_file.read()
-        >>> body = base64.b64encode(img_data).decode("utf-8")
-        >>> with ImageToText(api_key="CAI-12345....") as image_to_text_inst:
-        ...    image_to_text_inst.captcha_handler(body=body)
-        CaptchaResponseSer(errorId=0,
-                           errorCode=None,
-                           errorDescription=None,
-                           taskId='73bdcd28-6c77-4414-8....',
-                           status=<ResponseStatusEnm.Ready: 'ready'>,
-                           solution={'confidence': 0.9585, 'text': 'gcphjd'}
-                          )
+        >>> body = FileInstrument().file_processing(captcha_file="captcha_example.jpeg")
+        >>> asyncio.run(ImageToText(api_key="CAI-12345....").aio_captcha_handler(
+        >>>                     task_payload={"body": body, "module": "common"}
+        >>>                     )
+        >>>         )
+        {
+           "errorId":0,
+           "errorCode":"None",
+           "errorDescription":"None",
+           "taskId":"db0a3153-621d-4f5e-8554-a1c032597ee7",
+           "status":"ready",
+           "solution":{
+              "confidence":0.9585,
+              "text":"gcphjd"
+           }
+        }
 
-        >>> with open('some_image.jpeg', 'rb') as img_file:
-        ...    img_data = img_file.read()
-        >>> body = base64.b64encode(img_data).decode("utf-8")
-        >>> await ImageToText(api_key="CAI-12345....").aio_captcha_handler(body=body)
-        CaptchaResponseSer(errorId=0,
-                           errorCode=None,
-                           errorDescription=None,
-                           taskId='73bdcd28-6c77-4414-8....',
-                           status=<ResponseStatusEnm.Ready: 'ready'>,
-                           solution={'confidence': 0.9585, 'text': 'gcphjd'}
-                          )
+        >>> from python3_capsolver.image_to_text import ImageToText
+        >>> from python3_capsolver.core.captcha_instrument import FileInstrument
 
-        >>> with open('some_image.jpeg', 'rb') as img_file:
-        ...    img_data = img_file.read()
-        >>> body = base64.b64encode(img_data).decode("utf-8")
-        >>> with ImageToText(api_key="CAI-12345....") as image_to_text_inst:
-        ...    await image_to_text_inst.aio_captcha_handler(body=body)
-        CaptchaResponseSer(errorId=0,
-                           errorCode=None,
-                           errorDescription=None,
-                           taskId='73bdcd28-6c77-4414-8....',
-                           status=<ResponseStatusEnm.Ready: 'ready'>,
-                           solution={'confidence': 0.9585, 'text': 'gcphjd'}
-                          )
+        >>> body = FileInstrument().file_processing(captcha_file="captcha_example.jpeg")
+        >>> ImageToText(api_key="CAI-12345....")captcha_handler(
+        >>>                     task_payload={"body": body,
+        >>>                                     "module": "common",
+        >>>                                     "score": 0.92}
+        >>>                     )
+        {
+           "errorId":0,
+           "errorCode":"None",
+           "errorDescription":"None",
+           "taskId":"db0a3153-621d-4f5e-8554-a1c032597ee7",
+           "status":"ready",
+           "solution":{
+              "confidence":0.9585,
+              "text":"gcphjd"
+           }
+        }
 
     Returns:
-        CaptchaResponseSer model with full server response
+        Dict with full server response
 
     Notes:
         https://docs.capsolver.com/guide/recognition/ImageToTextTask.html
     """
 
-    def __init__(
-        self,
-        captcha_type: Union[ImageToTextTaskTypeEnm, str] = ImageToTextTaskTypeEnm.ImageToTextTask,
-        *args,
-        **kwargs,
-    ):
-        super().__init__(*args, **kwargs)
-        if captcha_type in ImageToTextTaskTypeEnm.list():
-            self.task_params = TaskSer(**locals()).dict(exclude_none=True)
-        else:
-            raise ValueError(
-                f"""Invalid `captcha_type` parameter set for `{self.__class__.__name__}`,
-                available - {ImageToTextTaskTypeEnm.list()}"""
-            )
-        for key in kwargs:
-            self.task_params.update({key: kwargs[key]})
+    def __init__(self, api_key: str, **kwargs):
 
-    def captcha_handler(self, body: str) -> CaptchaResponseSer:
+        super().__init__(api_key=api_key, captcha_type=CaptchaTypeEnm.ImageToTextTask, **kwargs)
+
+    def captcha_handler(self, task_payload: Dict) -> Dict[str, str]:
         """
         Synchronous method for captcha solving
 
         Args:
-            body: Base64 encoded content of the image, decoded into str
+            task_payload: Captcha solving `task` payload, include `body`, `module` and other fields.
 
         Returns:
-            CaptchaResponseSer model with full server response
+            Dict with full server response
 
         Notes:
             Check class docstring for more info
         """
-        self.task_params.update({"body": body})
-        return self._processing_captcha(create_params=self.task_params)
+        task_payload.pop("type", None)
+        self.task_params.update(task_payload)
+        self._captcha_handling_instrument = SIOCaptchaInstrument(captcha_params=self)
+        return self._captcha_handling_instrument.processing_captcha()
 
-    async def aio_captcha_handler(self, body: str) -> CaptchaResponseSer:
+    async def aio_captcha_handler(self, task_payload: Dict) -> Dict[str, str]:
         """
         Asynchronous method for captcha solving
 
         Args:
-            body: Base64 encoded content of the image, decoded into str
+            task_payload: Captcha solving `task` payload, include `body`, `module` and other fields.
 
         Returns:
-            CaptchaResponseSer model with full server response
+            Dict with full server response
 
         Notes:
             Check class docstring for more info
         """
-        self.task_params.update({"body": body})
-        return await self._aio_processing_captcha(create_params=self.task_params)
+        task_payload.pop("type", None)
+        self.task_params.update(task_payload)
+        self._captcha_handling_instrument = AIOCaptchaInstrument(captcha_params=self)
+        return await self._captcha_handling_instrument.processing_captcha()
