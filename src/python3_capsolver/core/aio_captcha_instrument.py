45f0de091e3bf84001ade77ecd113d7c7dff2f29
Andrei <drop.all.requests@gmail.com>
2025-01-18 02:42:00 +0300

    Update aio_captcha_instrument.py

--- src/python3_capsolver/core/aio_captcha_instrument.py
+++ src/python3_capsolver/core/aio_captcha_instrument.py
@@ -31,8 +31,9 @@ class AIOCaptchaInstrument(CaptchaInstrument):
         # if task created and already ready - return result
         if self.created_task_data.errorId == 0 and self.created_task_data.status == ResponseStatusEnm.Processing.value:
             return (await self.__get_result()).to_dict()
+        elif self.created_task_data.errorId != 0:
+            self.created_task_data.status = ResponseStatusEnm.Failed
 
-        self.created_task_data.status = ResponseStatusEnm.Failed
         return self.created_task_data.to_dict()
 
     async def __create_task(self, url_postfix: str = EndpointPostfixEnm.CREATE_TASK.value) -> dict:
